03ad2fdf0f15318a7fa491d047e367ac523b88f6
Toon Van den Bos <toon@whitecube.be>
2025-02-28 10:26:33 +0100

    Added travel rating data injection (get_field)

--- wp-content/themes/dw/single-travel.php
+++ wp-content/themes/dw/single-travel.php
@@ -129,8 +129,8 @@
                 <div class="travel__head">
                     <h2 class="travel__title"><?= get_the_title(); ?></h2>
                     <p class="travel__excerpt"><?= get_the_excerpt(); ?></p>
-                    <div class="travel__rating" data-score="4">
-                        <p class="sro">Ce voyage obtient l'appréciation de 4 étoiles sur 5</p>
+                    <div class="travel__rating" data-score="<?= get_field('rating'); ?>">
+                        <p class="sro">Ce voyage obtient l'appréciation de <?= get_field('rating'); ?> étoiles sur 5</p>
                     </div>
                 </div>
                 <figure class="travel__back">
