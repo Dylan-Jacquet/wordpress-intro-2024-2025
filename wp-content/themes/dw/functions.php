a9d7b58f5f1decf7a3e0ea2ef56348c59d049790
Toon Van den Bos <toon@whitecube.be>
2025-02-28 09:33:01 +0100

    Added image sizes

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -48,6 +48,26 @@ register_post_type('travel', [
     'supports' => ['title','excerpt','editor','thumbnail'],
 ]);
 
+// Paramétrer des tailles d'images pour le générateur de thumbnails de Wordpress :
+
+// Sans recadrage :
+add_image_size('travel-side', 420, 420); 
+// Avec recadrage :
+add_image_size('travel-header', 1920, 400, true);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- wp-content/themes/dw/single-travel.php
+++ wp-content/themes/dw/single-travel.php
@@ -2,6 +2,49 @@
 
 <style type="text/css">
     .travel {
+    }
+    .travel__header {
+        height: 400px;
+        width: 100%;
+        position: relative;
+    }
+    .travel__back,
+    .travel__back:before,
+    .travel__head {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
+    }
+    .travel__back {
+        z-index: 0;
+        margin: 0;
+        padding: 0;
+    }
+    .travel__back:before {
+        content: '';
+        display: block;
+        background: rgb(100,20,40);
+        opacity: 0.75;
+    }
+    .travel__cover {
+        display: block;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+    .travel__head {
+        z-index: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: white;
+    }
+    .travel__container {
         display: flex;
         flex-direction: row-reverse;
         justify-content: space-between;
@@ -36,28 +79,33 @@
     // On ouvre "la boucle" (The Loop), la structure de contrôle
     // de contenu propre à Wordpress:
     if(have_posts()): while(have_posts()): the_post(); ?>
-
-        <h2><?= get_the_title(); ?></h2>
-
-        <p><?= get_the_excerpt(); ?></p>
-
         <div class="travel">
-
-            <aside class="travel__ingredients">
-                <div>
-                    <h3>Aperçu</h3>
-                    <p>À compléter</p>
+            <header class="travel__header">
+                <div class="travel__head">
+                    <h2 class="travel__title"><?= get_the_title(); ?></h2>
+                    <p class="travel__excerpt"><?= get_the_excerpt(); ?></p>
                 </div>
-                <figure class="travel__fig">
-                    <?= get_the_post_thumbnail(size: 'large', attr: ['class' => 'travel__img']); ?>
+                <figure class="travel__back">
+                    <?= get_the_post_thumbnail(size: 'travel-header', attr: ['class' => 'travel__cover']); ?>
                 </figure>
-            </aside>
+            </header>
 
-            <section class="travel__steps">
-                <h3>Récit de voyage</h3>
-                <div><?= get_the_content(); ?></div>
-            </section>
+            <div class="travel__container">
+                <aside class="travel__ingredients">
+                    <div>
+                        <h3>Aperçu</h3>
+                        <p>À compléter</p>
+                    </div>
+                    <figure class="travel__fig">
+                        <?= get_the_post_thumbnail(size: 'travel-side', attr: ['class' => 'travel__img']); ?>
+                    </figure>
+                </aside>
 
+                <section class="travel__steps">
+                    <h3>Récit de voyage</h3>
+                    <div><?= get_the_content(); ?></div>
+                </section>
+            </div>
         </div>
 
     <?php
