bc82776997a282e7dadab97e7f54782e7d9a1093
Toon Van den Bos <toon@whitecube.be>
2025-03-21 09:35:29 +0100

    Added contact form using Contact Form 7 plugin & do_shortcode

--- wp-content/themes/dw/template-contact.php
+++ wp-content/themes/dw/template-contact.php
@@ -9,7 +9,12 @@
     // de contenu propre à Wordpress:
     if(have_posts()): while(have_posts()): the_post(); ?>
 
-        <div><?= get_the_content(); ?></div>
+    <section class="contact">
+        <div class="contact__left"><?= get_the_content(); ?></div>
+        <div class="contact__right">
+            <?= do_shortcode('[contact-form-7 id="c40b496" title="Formulaire page contact"]'); ?>
+        </div>
+    </section>
 
     <?php
     // On ferme "la boucle" (The Loop):
