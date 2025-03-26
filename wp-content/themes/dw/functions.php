002166370e9b8e02c3d3972509aec9654ce7073e
Dylan Jacquet <dylanjacquet235@gmail.com>
2025-03-26 19:17:21 +0100

    fix : Remove stylesheet and script for back-office

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -112,7 +112,7 @@ function enqueue_assets_from_vite_manifest(): void
     }
   }
 }
-enqueue_assets_from_vite_manifest();
+//enqueue_assets_from_vite_manifest();
 
 // Function utilitaire permettant de charger le fichier contenant tous les imports de police hors du css.
 function dw_fonts(): string
