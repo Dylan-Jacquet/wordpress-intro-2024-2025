fd60e9b783a4dbd8f90eb3fa7a023422a667c40c
Dylan Jacquet <dylanjacquet235@gmail.com>
2025-03-26 19:15:08 +0100

    chore : Updated Vite configuration / Installed PostCSS for Autoprefixer / Added a function in functions.php to serve data excluding the WordPress back-end / Added a function for importing fonts into a separate CSS file

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -5,7 +5,7 @@ include_once('fields.php');
 
 // Vérifier si la session est active ("started") ?
 if (session_status() === PHP_SESSION_NONE) {
-    session_start();
+  session_start();
 }
 
 /**
@@ -63,19 +63,28 @@ function get__option($field): mixed
 // allons créer. On va donc le désactiver :
 
 // Disable Gutenberg on the back end.
-add_filter( 'use_block_editor_for_post', '__return_false' );
+add_filter('use_block_editor_for_post', '__return_false');
 // Disable Gutenberg for widgets.
-add_filter( 'use_widgets_block_editor', '__return_false' );
+add_filter('use_widgets_block_editor', '__return_false');
 
 // Disable default front-end styles.
-add_action( 'wp_enqueue_scripts', function() {
-    // Remove CSS on the front end.
-    wp_dequeue_style( 'wp-block-library' );
-    // Remove Gutenberg theme.
-    wp_dequeue_style( 'wp-block-library-theme' );
-    // Remove inline global CSS on the front end.
-    wp_dequeue_style( 'global-styles' );
-}, 20 );
+add_action('wp_enqueue_scripts', function () {
+  // Remove CSS on the front end.
+  wp_dequeue_style('wp-block-library');
+  // Remove Gutenberg theme.
+  wp_dequeue_style('wp-block-library-theme');
+  // Remove inline global CSS on the front end.
+  wp_dequeue_style('global-styles');
+}, 20);
+
+add_action('init', 'init_remove_support', 100);
+
+function init_remove_support(): void
+{
+  remove_post_type_support('post', 'editor');
+  remove_post_type_support('page', 'editor');
+  remove_post_type_support('product', 'editor');
+}
 
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
@@ -85,74 +94,120 @@ remove_action('wp_head', 'wp_oembed_add_host_js');
 remove_action('wp_head', 'rest_output_link_wp_head');
 remove_action('wp_head', 'wp_generator');
 
-$manifestPath = get_theme_file_path('public/.vite/manifest.json');
+function enqueue_assets_from_vite_manifest(): void
+{
+  $manifestPath = get_theme_file_path('public/.vite/manifest.json');
 
-if (file_exists($manifestPath)) {
-  $manifest = json_decode(file_get_contents($manifestPath), true);
+  if (file_exists($manifestPath)) {
+    $manifest = json_decode(file_get_contents($manifestPath), true);
 
-  if (isset($manifest['wp-content/themes/dw/resources/js/main.js'])) {
-    wp_enqueue_script('dw', get_theme_file_uri('public/' . $manifest['wp-content/themes/dw/resources/js/main.js']['file']), [], null, true);
-  }
+    // Vérifier et ajouter le fichier JavaScript
+    if (isset($manifest['wp-content/themes/dw/resources/js/main.js'])) {
+      wp_enqueue_script('dw', get_theme_file_uri('public/' . $manifest['wp-content/themes/dw/resources/js/main.js']['file']), [], null, true);
+    }
 
-  if (isset($manifest['wp-content/themes/dw/resources/css/styles.scss'])) {
-    wp_enqueue_style('dw', get_theme_file_uri('public/' . $manifest['wp-content/themes/dw/resources/css/styles.scss']['file']));
+    // Vérifier et ajouter le fichier CSS
+    if (isset($manifest['wp-content/themes/dw/resources/css/styles.scss'])) {
+      wp_enqueue_style('dw', get_theme_file_uri('public/' . $manifest['wp-content/themes/dw/resources/css/styles.scss']['file']));
+    }
   }
 }
+enqueue_assets_from_vite_manifest();
+
+// Function utilitaire permettant de charger le fichier contenant tous les imports de police hors du css.
+function dw_fonts(): string
+{
+  return get_template_directory_uri() . '/resources/fonts/fonts.css';
+}
+
+// 1. Charger un fichier "public" (asset/image/css/script/...) pour le front-end sans que cela ne s'applique à l'admin.
+function dw_asset(string $file): string
+{
+  $manifestPath = get_theme_file_path('public/.vite/manifest.json');
+
+  if (file_exists($manifestPath)) {
+    $manifest = json_decode(file_get_contents($manifestPath), true);
+
+    if (isset($manifest['wp-content/themes/dw/resources/js/main.js']) && $file === 'js') {
+      return get_theme_file_uri('public/' . $manifest['wp-content/themes/dw/resources/js/main.js']['file']);
+    }
+
+    if (isset($manifest['wp-content/themes/dw/resources/css/styles.scss']) && $file === 'css') {
+      return get_theme_file_uri('public/' . $manifest['wp-content/themes/dw/resources/css/styles.scss']['file']);
+    }
+  }
+
+  return get_template_directory_uri() . '/public/' . $file;
+}
+
+// Permet d'ajouter la possibilité d'uploader des extensions de fichiers non compatibles de base.
+// Exemple : ici nous ajoutons le format SVG en tant que type d'image compatible pour l'upload.
+function my_own_mime_types($mimes)
+{
+  // Ajout du mime type pour les fichiers SVG
+  $mimes['svg'] = 'image/svg+xml';
+
+  // Retourne le tableau des types MIME mis à jour
+  return $mimes;
+}
+
+// Ajoute notre fonction de filtrage à l'action 'upload_mimes' pour permettre l'upload des fichiers SVG.
+add_filter('upload_mimes', 'my_own_mime_types');
 
 // Activer l'utilisation des vignettes (image de couverture) sur nos post types:
-add_theme_support('post-thumbnails', ['recipe','travel']);
+add_theme_support('post-thumbnails', ['recipe', 'travel']);
 
 // Enregistrer de nouveaux "types de contenu", qui seront stockés dans la table
 // "wp_posts", avec un identifiant de type spécifique dans la colonne "post_type":
 
 register_post_type('recipe', [
-    'label' => 'Recettes',
-    'description' => 'Les recettes liées à nos voyages',
-    'menu_position' => 7,
-    'menu_icon' => 'dashicons-carrot',
-    'public' => true,
-    'has_archive' => true,
-    'rewrite' => [
-        'slug' => 'recettes',
-    ],
-    'supports' => ['title','excerpt','editor','thumbnail'],
+  'label' => 'Recettes',
+  'description' => 'Les recettes liées à nos voyages',
+  'menu_position' => 7,
+  'menu_icon' => 'dashicons-carrot',
+  'public' => true,
+  'has_archive' => true,
+  'rewrite' => [
+    'slug' => 'recettes',
+  ],
+  'supports' => ['title', 'excerpt', 'editor', 'thumbnail'],
 ]);
 
 register_post_type('travel', [
-    'label' => 'Voyages',
-    'description' => 'Les voyages que nous avons réalisés',
-    'menu_position' => 6,
-    'menu_icon' => 'dashicons-airplane',
-    'public' => true,
-    'has_archive' => true,
-    'rewrite' => [
-        'slug' => 'voyages',
-    ],
-    'supports' => ['title','excerpt','editor','thumbnail'],
+  'label' => 'Voyages',
+  'description' => 'Les voyages que nous avons réalisés',
+  'menu_position' => 6,
+  'menu_icon' => 'dashicons-airplane',
+  'public' => true,
+  'has_archive' => true,
+  'rewrite' => [
+    'slug' => 'voyages',
+  ],
+  'supports' => ['title', 'excerpt', 'editor', 'thumbnail'],
 ]);
 
 // Ajouter des "catégories" (taxonomies) sur ces post_types :
 
 register_taxonomy('course', ['recipe'], [
-    'labels' => [
-        'name' => 'Services',
-        'singular_name' => 'Service'
-    ],
-    'description' => 'À quel moment du repas ce plat intervient-il ?',
-    'public' => true,
-    'hierarchical' => true,
-    'show_tagcloud' => false,
+  'labels' => [
+    'name' => 'Services',
+    'singular_name' => 'Service'
+  ],
+  'description' => 'À quel moment du repas ce plat intervient-il ?',
+  'public' => true,
+  'hierarchical' => true,
+  'show_tagcloud' => false,
 ]);
 
 register_taxonomy('diet', ['recipe'], [
-    'labels' => [
-        'name' => 'Régimes alimentaires',
-        'singular_name' => 'Régime'
-    ],
-    'description' => 'À quel type de régime appartient cette recette ?',
-    'public' => true,
-    'hierarchical' => true,
-    'show_tagcloud' => false,
+  'labels' => [
+    'name' => 'Régimes alimentaires',
+    'singular_name' => 'Régime'
+  ],
+  'description' => 'À quel type de régime appartient cette recette ?',
+  'public' => true,
+  'hierarchical' => true,
+  'show_tagcloud' => false,
 ]);
 
 // Paramétrer des tailles d'images pour le générateur de thumbnails de Wordpress :
@@ -172,44 +227,44 @@ register_nav_menu('footer', 'Le menu de navigation de fin de page.');
 
 function dw_get_navigation_links(string $location): array
 {
-    // Récupérer l'objet WP pour le menu à la location $location
-    $locations = get_nav_menu_locations();
+  // Récupérer l'objet WP pour le menu à la location $location
+  $locations = get_nav_menu_locations();
 
-    if(! isset($locations[$location])) {
-        return [];
-    }
+  if (!isset($locations[$location])) {
+    return [];
+  }
 
-    $nav_id = $locations[$location];
-    $nav = wp_get_nav_menu_items($nav_id);
+  $nav_id = $locations[$location];
+  $nav = wp_get_nav_menu_items($nav_id);
 
-    // Transformer le menu en un tableau de liens, chaque lien étant un objet personnalisé
+  // Transformer le menu en un tableau de liens, chaque lien étant un objet personnalisé
 
-    $links = [];
+  $links = [];
 
-    foreach ($nav as $post) {
-        $link = new stdClass();
-        $link->href = $post->url;
-        $link->label = $post->title;
-        $link->icon = get_field('icon', $post);
+  foreach ($nav as $post) {
+    $link = new stdClass();
+    $link->href = $post->url;
+    $link->label = $post->title;
+    $link->icon = get_field('icon', $post);
 
-        $links[] = $link;
-    }
+    $links[] = $link;
+  }
 
-    // Retourner ce tableau d'objets (liens).
-    return $links;
+  // Retourner ce tableau d'objets (liens).
+  return $links;
 }
 
 // Ajouter un post-type custom pour sauvegarder les messages de contact
 
 register_post_type('contact_message', [
-    'label' => 'Messages de contact',
-    'description' => 'Les envois de formulaire via la page de contact',
-    'menu_position' => 10,
-    'menu_icon' => 'dashicons-email',
-    'public' => false,
-    'show_ui' => true,
-    'has_archive' => false,
-    'supports' => ['title','editor'],
+  'label' => 'Messages de contact',
+  'description' => 'Les envois de formulaire via la page de contact',
+  'menu_position' => 10,
+  'menu_icon' => 'dashicons-email',
+  'public' => false,
+  'show_ui' => true,
+  'has_archive' => false,
+  'supports' => ['title', 'editor'],
 ]);
 
 // Ajouter la fonctionnalité "POST" pour un formulaire de contact personnalisé :
@@ -217,45 +272,24 @@ add_action('admin_post_dw_submit_contact_form', 'dw_handle_contact_form');
 add_action('admin_post_nopriv_dw_submit_contact_form', 'dw_handle_contact_form');
 
 // Chargement de notre class qui va gérer ce formulaire
-require_once(__DIR__.'/forms/ContactForm.php');
+require_once(__DIR__ . '/forms/ContactForm.php');
 
 function dw_handle_contact_form()
 {
-    $form = (new \DW_Theme\Forms\ContactForm())
-        ->rule('firstname', 'required')
-        ->rule('lastname', 'required')
-        ->rule('email', 'required')
-        ->rule('email', 'email')
-        ->rule('message', 'required')
-        ->rule('message', 'no_test')
-        ->sanitize('firstname', 'sanitize_text_field')
-        ->sanitize('lastname', 'sanitize_text_field')
-        ->sanitize('email', 'sanitize_text_field')
-        ->sanitize('message', 'sanitize_textarea_field');
+  $form = (new \DW_Theme\Forms\ContactForm())
+    ->rule('firstname', 'required')
+    ->rule('lastname', 'required')
+    ->rule('email', 'required')
+    ->rule('email', 'email')
+    ->rule('message', 'required')
+    ->rule('message', 'no_test')
+    ->sanitize('firstname', 'sanitize_text_field')
+    ->sanitize('lastname', 'sanitize_text_field')
+    ->sanitize('email', 'sanitize_text_field')
+    ->sanitize('message', 'sanitize_textarea_field');
 
-    return $form->handle($_POST);
+  return $form->handle($_POST);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Créer une fonction qui permet de créer des pages d'options ACF pour le thème :
 function create_site_options_page(): void
@@ -263,20 +297,20 @@ function create_site_options_page(): void
   if (function_exists('acf_add_options_page')) {
     // Page principale
     acf_add_options_page([
-      'page_title'  => 'Site Options',
-      'menu_title'  => 'Site Settings',
-      'menu_slug'   => 'site-options',
-      'capability'  => 'edit_posts',
-      'redirect'    => false
+      'page_title' => 'Site Options',
+      'menu_title' => 'Site Settings',
+      'menu_slug' => 'site-options',
+      'capability' => 'edit_posts',
+      'redirect' => false
     ]);
 
     foreach (['fr', 'en'] as $lang) {
       acf_add_options_sub_page([
         'page_title' => sprintf(__('Options du site %s', 'hepl-trad'), strtoupper($lang)),
         'menu_title' => sprintf(__('Options du site %s', 'hepl-trad'), strtoupper($lang)),
-        'menu_slug'  => 'site-options-' . $lang,
-        'post_id'    => $lang,
-        'parent'     => 'site-options',
+        'menu_slug' => 'site-options-' . $lang,
+        'post_id' => $lang,
+        'parent' => 'site-options',
       ]);
     }
   }
