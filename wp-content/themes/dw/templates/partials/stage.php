b03d68937f83afc9bce5a91fed9d93e0773e7e8d
Dylan Jacquet <dylanjacquet235@gmail.com>
2025-04-04 11:14:31 +0200

    chore : Add stage template with css classes

--- wp-content/themes/dw/templates/partials/stage.php
+++ wp-content/themes/dw/templates/partials/stage.php
@@ -1,18 +1,24 @@
 <?php $stage = get_field('stage') ?>
-<p>
-  <?= $stage['supline'] ?>
-</p>
-<h1>
-  <?= $stage['headline'] ?>
-</h1>
-<p>
-  <?= $stage['subline'] ?>
-</p>
-<a href="<?= $stage['link']['url'] ?>">
-  <?= $stage['link']['title'] ?>
-</a>
+<section class="stage">
+  <p class="stage__supline">
+    <?= $stage['supline'] ?>
+  </p>
+  <h1 class="stage__headline">
+    <?= $stage['headline'] ?>
+  </h1>
+  <p class="stage__subline">
+    <?= $stage['subline'] ?>
+  </p>
+  <div class="stage__description">
+    <?= $stage['text'] ?>
+  </div>
+  <a class="stage__link" href="<?= $stage['link']['url'] ?>">
+    <?= $stage['link']['title'] ?>
+  </a>
 
-<img src="<?= $stage['background_image']['url'] ?>"
-     alt="<?= $stage['background_image']['alt'] ?>"
-     width="<?= $stage['background_image']['width'] ?>"
-     height="<?= $stage['background_image']['height'] ?>"/>
+  <img class="stage__image"
+       src="<?= $stage['background_image']['url'] ?>"
+       alt="<?= $stage['background_image']['alt'] ?>"
+       width="<?= $stage['background_image']['width'] ?>"
+       height="<?= $stage['background_image']['height'] ?>"/>
+</section>
