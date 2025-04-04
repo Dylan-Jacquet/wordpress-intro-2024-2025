e03d925d915b43d1e5e95a0ecf3555d0235c3eef
Dylan Jacquet <dylanjacquet235@gmail.com>
2025-04-04 11:08:31 +0200

    chore : Add scss file and text-media.php

--- wp-content/themes/dw/templates/content/text-media/text-media.php
+++ wp-content/themes/dw/templates/content/text-media/text-media.php
@@ -1,11 +1,39 @@
 <?php $headline = get_sub_field('headline') ?>
 <?php $text = get_sub_field('text') ?>
 <?php $link = get_sub_field('link') ?>
+<?php $image = get_sub_field('background_image') ?>
 <?php $media_position = get_sub_field('media_position') ?>
 <?php $media_type = get_sub_field('media_type') ?>
 
-<?= var_dump($headline) ?>
-<?= var_dump($text) ?>
-<?= var_dump($link) ?>
-<?= var_dump($media_position) ?>
-<?= var_dump($media_type) ?>
+<section class="text-media text-media__position--<?= $media_position ?>">
+  <div class="text-media__content-container">
+    <?php if ($headline !== ""): ?>
+      <h2 class="text-media__content-title">
+        <?= $headline ?>
+      </h2>
+    <?php endif; ?>
+    <div class="text-media__content-text">
+      <?= $text ?>
+    </div>
+    <a class="text-media__content-link"
+       href="<?= $link['url'] ?>"
+       target="<?= $link['target'] === "_blank" ?: "_self" ?>">
+      <?= $link['title'] ?>
+    </a>
+  </div>
+  <?php if ($media_type === 'image'): ?>
+    <img class="text-media__image" src="<?= $image['url'] ?>"
+         alt="<?= $image['alt'] ?>"
+         width="<?= $image['width'] ?>"
+         height="<?= $image['height'] ?>">
+  <?php elseif ($media_type === 'video'): ?>
+    <video class="text-media__video">
+
+    </video>
+  <?php elseif ($media_type === 'slider'): ?>
+    <div>
+
+    </div>
+  <?php endif; ?>
+</section>
+
